f9c967c2420ef03a586aa59311cfe65cb4865649
JavaUnchained <AndreyVladimirov1ch@yandex.ru>
2024-10-09 13:21:15 +0300

    rm unused imports

--- src/main/java/com/cdpo/techservice/controller/ServiceController.java
+++ src/main/java/com/cdpo/techservice/controller/ServiceController.java
@@ -5,8 +5,6 @@ import com.cdpo.techservice.dto.ServiceResponseDTO;
 import com.cdpo.techservice.repository.IServiceRepository;
 import com.cdpo.techservice.repository.ServiceRepository;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,7 +13,6 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
 
 
 @Validated
