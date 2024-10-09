3145b19e017a9db8a7afc36572c503aa75decfec
JavaUnchained <AndreyVladimirov1ch@yandex.ru>
2024-10-09 15:16:22 +0300

    create service and repository

---
+++ src/main/java/com/cdpo/techservice/service/ServiceService.java
@@ -0,0 +1,55 @@
+package com.cdpo.techservice.service;
+
+import com.cdpo.techservice.dto.ServiceRequestDTO;
+import com.cdpo.techservice.dto.ServiceResponseDTO;
+import com.cdpo.techservice.model.Service;
+import com.cdpo.techservice.repository.IServiceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Primary
+@Repository
+@RequiredArgsConstructor
+public class ServiceService implements IServiceService {
+    private final IServiceRepository serviceRepository;
+    @Override
+    public long createService(ServiceRequestDTO service) {
+        return serviceRepository.createService(requestDtoToService(service));
+    }
+    @Override
+    public List<ServiceResponseDTO> getAllServices() {
+        return serviceRepository.getAllServices().stream()
+                .map(ServiceService::serviceToResponseDTO).collect(Collectors.toList());
+    }
+
+    @Override
+    public Optional<ServiceResponseDTO> getServiceById(long id) {
+        return Optional.ofNullable(serviceRepository.getServiceById(id))
+                .map(ServiceService::serviceToResponseDTO);
+    }
+
+    @Override
+    public Optional<ServiceResponseDTO> updateService(long id, ServiceRequestDTO updates) {
+        return Optional.ofNullable(
+                serviceRepository.updateServiceById(id, requestDtoToService(updates))
+        ).map(ServiceService::serviceToResponseDTO);
+    }
+
+    @Override
+    public boolean deleteService(long id) {
+        return serviceRepository.deleteServiceById(id);
+    }
+
+    private static ServiceResponseDTO serviceToResponseDTO(Service s) {
+        return new ServiceResponseDTO(s.getId(), s.getName(), s.getDescription());
+    }
+
+    private static Service requestDtoToService(ServiceRequestDTO service) {
+        return new Service(service.name(), service.description());
+    }
+}
