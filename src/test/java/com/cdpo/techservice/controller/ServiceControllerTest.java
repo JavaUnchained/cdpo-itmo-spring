2daa9da012f15ccbed171108b489f118656df8f5
JavaUnchained <AndreyVladimirov1ch@yandex.ru>
2024-10-14 20:53:13 +0300

    fix home work 2

--- src/test/java/com/cdpo/techservice/controller/ServiceControllerTest.java
+++ src/test/java/com/cdpo/techservice/controller/ServiceControllerTest.java
@@ -29,8 +29,7 @@ class ServiceControllerTest {
     public void test_create_service_with_valid_data() {
         ServiceSimpleRepository serviceSimpleRepository = new ServiceSimpleRepository();
         ServiceService serviceRepository = new ServiceService(serviceSimpleRepository);
-        ServiceBookingService serviceBookingService = new ServiceBookingService(new ServiceBookingSimpleRepository(serviceSimpleRepository));
-        ServiceController serviceController = new ServiceController(serviceRepository, serviceBookingService);
+        ServiceController serviceController = new ServiceController(serviceRepository);
         ServiceRequestDTO serviceRequest = new ServiceRequestDTO("Test Service", "Test Description", 60*60*10, 120);
 
         ResponseEntity<Long> response = serviceController.createService(serviceRequest);
@@ -43,9 +42,8 @@ class ServiceControllerTest {
     @Test
     public void test_retrieve_all_services() {
         IServiceService mockServiceService = mock(IServiceService.class);
-        IServiceBookingService mockBookService = mock(IServiceBookingService.class);
 
-        ServiceController serviceController = new ServiceController(mockServiceService, mockBookService);
+        ServiceController serviceController = new ServiceController(mockServiceService);
 
         List<ServiceResponseDTO> mockServiceList = new ArrayList<>();
         mockServiceList.add(new ServiceResponseDTO(1L, new ServiceRequestDTO("Service 1", "Description 1", 10, 100)));
@@ -63,8 +61,7 @@ class ServiceControllerTest {
     @Test
     public void test_update_service_with_valid_data() {
         IServiceService mockServiceRepository = mock(IServiceService.class);
-        IServiceBookingService mockBookService = mock(IServiceBookingService.class);
-        ServiceController serviceController = new ServiceController(mockServiceRepository, mockBookService);
+        ServiceController serviceController = new ServiceController(mockServiceRepository);
 
         long id = 1L;
         ServiceRequestDTO updates = new ServiceRequestDTO("Updated Service", "Updated Description", 1000, 200);
@@ -83,8 +80,7 @@ class ServiceControllerTest {
     @Test
     public void test_delete_service_with_valid_id_returns_204() {
         IServiceService mockServiceRepository = mock(IServiceService.class);
-        IServiceBookingService mockBookService = mock(IServiceBookingService.class);
-        ServiceController serviceController = new ServiceController(mockServiceRepository, mockBookService);
+        ServiceController serviceController = new ServiceController(mockServiceRepository);
         long validId = 1L;
 
         when(mockServiceRepository.deleteService(validId)).thenReturn(true);
@@ -98,8 +94,7 @@ class ServiceControllerTest {
     @Test
     public void test_delete_non_existent_service_returns_404() {
         IServiceService mockServiceRepository = mock(IServiceService.class);
-        IServiceBookingService mockBookService = mock(IServiceBookingService.class);
-        ServiceController serviceController = new ServiceController(mockServiceRepository, mockBookService);
+        ServiceController serviceController = new ServiceController(mockServiceRepository);
 
         long nonExistentId = 9999L;
         when(mockServiceRepository.deleteService(nonExistentId)).thenReturn(false);
@@ -113,8 +108,7 @@ class ServiceControllerTest {
     @Test
     public void test_update_non_existent_service_returns_404() {
         IServiceService serviceRepository = mock(IServiceService.class);
-        IServiceBookingService mockBookService = mock(IServiceBookingService.class);
-        ServiceController serviceController = new ServiceController(serviceRepository, mockBookService);
+        ServiceController serviceController = new ServiceController(serviceRepository);
 
         long nonExistentId = 9999L;
         ServiceRequestDTO updates = new ServiceRequestDTO("Updated Name", "Updated Description", 100000, 123);
@@ -128,8 +122,7 @@ class ServiceControllerTest {
     @Test
     public void test_retrieve_non_existent_service_returns_404() {
         IServiceService mockServiceRepository = mock(IServiceService.class);
-        IServiceBookingService mockBookService = mock(IServiceBookingService.class);
-        ServiceController serviceController = new ServiceController(mockServiceRepository, mockBookService);
+        ServiceController serviceController = new ServiceController(mockServiceRepository);
 
         long nonExistentId = 1000L;
         when(mockServiceRepository.getServiceById(nonExistentId)).thenReturn(Optional.empty());
