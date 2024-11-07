c35c313b598030dbfe0375600937f44797610833
JavaUnchained <AndreyVladimirov1ch@yandex.ru>
2024-11-07 14:58:16 +0300

    implement

--- notifier/src/main/java/com/cdpo/notifier/repository/NotificationTemplateLocalRepository.java
+++ notifier/src/main/java/com/cdpo/notifier/repository/NotificationTemplateLocalRepository.java
@@ -1,4 +1,15 @@
 package com.cdpo.notifier.repository;
 
+import com.cdpo.notifier.dto.BookingStateDTO;
+
 public class NotificationTemplateLocalRepository implements INotificationTemplateRepository {
+    @Override
+    public String getBookingTemplate(BookingStateDTO bookingState, String firstName, String lastName) {
+        return null;
+    }
+
+    @Override
+    public String getDiscountTemplate(Double discontInPercent, String firstName, String lastName) {
+        return null;
+    }
 }
