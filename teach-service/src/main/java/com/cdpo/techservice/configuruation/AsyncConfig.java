824d416f4c4adb4bcddd9cfaccd209a3d4821bfe
JavaUnchained <AndreyVladimirov1ch@yandex.ru>
2024-11-07 18:29:14 +0300

    implement hw6

---
+++ teach-service/src/main/java/com/cdpo/techservice/configuruation/AsyncConfig.java
@@ -0,0 +1,10 @@
+package com.cdpo.techservice.configuruation;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+}
--- teach-service/src/main/java/com/cdpo/techservice/service/NotificationClientService.java
+++ teach-service/src/main/java/com/cdpo/techservice/service/NotificationClientService.java
@@ -3,28 +3,44 @@ package com.cdpo.techservice.service;
 import com.cdpo.techservice.client.NotificationClient;
 import com.cdpo.techservice.dto.NotificationBookingDTO;
 import com.cdpo.techservice.dto.NotificationDiscountDTO;
-import com.cdpo.techservice.exception.NotificationServiceException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+
 @Service
+@Slf4j
 @RequiredArgsConstructor
 @ConditionalOnProperty(name = "notification.enable", havingValue = "true")
 public class NotificationClientService implements INotificationClientService {
+    public static final String NOTIFICATION_SERVICE_EXCEPTION = "Notification Service Exception. Http code {}";
     private final NotificationClient notificationClient;
 
     @Override
+    @Async
     public void sendNotification(NotificationBookingDTO notification) {
-        if (!notificationClient.sendBookingNotification(notification).getStatusCode().is2xxSuccessful()) {
-            throw new NotificationServiceException();
-        }
+        CompletableFuture.runAsync(() ->{
+            HttpStatusCode statusCode = notificationClient.sendBookingNotification(notification).getStatusCode();
+            if (statusCode.is2xxSuccessful()) {
+              log.error(NOTIFICATION_SERVICE_EXCEPTION, statusCode);
+            }
+        });
     }
 
     @Override
+    @Async
     public void sendNotification(NotificationDiscountDTO notification) {
-        if (!notificationClient.sendDiscountNotification(notification).getStatusCode().is2xxSuccessful()) {
-            throw new NotificationServiceException();
-        }
+        CompletableFuture.runAsync(() ->{
+            HttpStatusCode statusCode = notificationClient.sendDiscountNotification(notification).getStatusCode();
+            if (!statusCode.is2xxSuccessful()) {
+                log.error(NOTIFICATION_SERVICE_EXCEPTION, statusCode);
+            }
+        });
+
     }
 
 }
